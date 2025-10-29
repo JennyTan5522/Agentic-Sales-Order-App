436d6b8804816310bede31197ae898ffa288bfca
JennyTan123 <85146823+JennyTan123@users.noreply.github.com>
2025-10-29 21:28:54 +0800

    chore(config): add base configuration and logging setup

---
+++ backend/config/service_config.py
@@ -0,0 +1,21 @@
+from pydantic import Field, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class ServiceConfig(BaseSettings):
+    """
+    Application service configuration loaded from environment variables or defaults.
+    """
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="allow")
+    ENV: str = Field(default="dev", description="Environment: dev, staging, prod")
+    LOG_LEVEL: str = Field(default="DEBUG", description="Logging level")
+    LOG_CONFIG_FILE: str = Field(default=f"./config/logging.yaml",description="Path to logging config file")
+
+    CLIENT_ID: SecretStr = Field(..., description="OAuth Client ID")
+    CLIENT_SECRET: SecretStr = Field(..., description="OAuth Client Secret")
+    TENANT_ID: SecretStr = Field(..., description="OAuth Tenant ID")
+    ONEDRIVE_EMAIL: str = Field(..., description="OneDrive Email")
+    BC_ENV_NAME: str = Field(..., description="Business Central Environment Name")
+    ONEDRIVE_DIR_PATH: str = Field(default="Documents/PO_FABRICS", description="OneDrive Directory Path for PO Fabrics")
+
+    OPENAI_API_KEY: SecretStr = Field(..., description="OpenAI API Key")
+    OPENAI_MODEL_NAME: str = Field(default="gpt-4o", description="OpenAI Model Name")
